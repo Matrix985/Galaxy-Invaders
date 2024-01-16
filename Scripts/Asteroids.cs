9ccf5deb1b8f3cb96dea891622e63621b9052aa7
Matrix985 <102406148+Matrix985@users.noreply.github.com>
2024-01-17 01:36:14 +0800

    Add files via upload

---
+++ Scripts/Asteroids.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Asteroids : MonoBehaviour {
+
+	private Vector3 screenPos;
+	private float asteroidTimer;
+
+	void OnEnable () {
+
+		screenPos = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width * 0.97f, 0, 0));
+		asteroidTimer = Time.time + 1;
+	}
+	
+	void Update () {
+
+		if (Time.time > asteroidTimer) {
+
+			asteroidTimer = Time.time + 3;
+			GameObject asteroid = ObjectPooler.instance.GetPooledObject (GameConstants.PooledObject.ASTEROID);
+			asteroid.transform.position = new Vector3 (Random.Range (-screenPos.x, screenPos.x), transform.position.y, transform.position.z);
+			float size = Random.Range (0.3f, 1.0f);
+			asteroid.transform.parent = transform;
+			asteroid.transform.localScale = new Vector3 (size, size, 1);
+		}
+	}
+}
